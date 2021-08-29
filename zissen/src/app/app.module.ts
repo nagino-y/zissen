6a5253eb6f2394d6f5a9abd56161cdc00d470958
Yukichi <naginoyukichi@gmail.com>
2021-08-29 19:01:58 +0900

    contents 追加

--- zissen/src/app/app.module.ts
+++ zissen/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { FirstviewComponent } from './firstview/firstview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FirstviewComponent
   ],
   imports: [
     BrowserModule,
