a2bb08926329a8c0a26caaeed7c153933e7c4b5a
Yukichi <naginoyukichi@gmail.com>
2021-09-02 23:27:15 +0900

    contents 追加３

---
+++ zissen/src/app/firstview/firstview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-firstview',
+  templateUrl: './firstview.component.html',
+  styleUrls: ['./firstview.component.scss']
+})
+export class FirstviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
